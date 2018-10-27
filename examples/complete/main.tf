1e639717ab36f7a34a3c09af2c2cd9aefe66f4f5
tmknom <tmk.nom@gmail.com>
2018-10-27 18:25:55 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,40 @@
 # Module Configuration
 #
 # https://www.terraform.io/docs/configuration/modules.html
+
+module "iam_role" {
+  source             = "../../"
+  name               = "complete"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  policy             = "${data.aws_iam_policy_document.policy.json}"
+
+  path        = "/ec2/"
+  description = "Describe EC2"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:Describe*",
+    ]
+
+    resources = ["*"]
+  }
+}
