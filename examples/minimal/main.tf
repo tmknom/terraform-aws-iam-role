3212979250d38d8f80a1c2a340f97e72f68842ed
tmknom <tmk.nom@gmail.com>
2018-10-27 12:06:21 +0900

    Implement minimal example

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,3 +1,37 @@
 # Module Configuration
 #
 # https://www.terraform.io/docs/configuration/modules.html
+
+module "iam_role" {
+  source             = "../../"
+  name               = "minimal"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  policy             = "${data.aws_iam_policy_document.policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:Describe*",
+    ]
+
+    resources = ["*"]
+  }
+}
